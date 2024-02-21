c911fe8ee7f1859aad42b1baa427323d58608cb6
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2024-02-22 01:02:04 +0200

    Update features

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
+import { useEffect, Suspense, lazy } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ThemeProvider } from "./context/ThemeContext";
-import GlobalStyles from "./styles/GlobalStyles";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import {Toaster} from "react-hot-toast"
-
-import Home from "./pages/Home";
-import ErrorPage from "./pages/ErrorPage";
-import AppLayout from "./ui/AppLayout";
-import { useEffect, useState } from "react";
-import Spinner from "./ui/Spinner";
-import { useDispatch } from "react-redux";
-import { getUser } from "./features/authentication/userSlice";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./features/authorization/ProtectedRoute";
+import GlobalStyles from "./styles/GlobalStyles";
 import { useSelector } from "react-redux";
-import Auth from "./pages/Auth";
-import LoginForm from "./features/authentication/LoginForm";
-import RegisterForm from "./features/authentication/RegisterForm";
-import Category from "./pages/Category";
-import CategoryProductList from "./features/category/CategoryProductList";
-import Product from "./pages/Product";
-import Search from "./pages/Search";
-import { updateFullCart } from "./features/cart/cartSlice";
-import WatchList from "./pages/WatchList";
-import { updateFullWatchlist } from "./features/watchlist/watchlistSlice";
-import Checkout from "./pages/Checkout";
+import { getUser } from "./features/authentication/userSlice";
 import useKeepAuthenticated from "./hooks/useKeepAuthenticated";
-import Profile from "./pages/Profile";
-import UserPanel from "./features/administration/UserPanel";
-import CategoryPanel from "./features/administration/CategoryPanel";
-import ProductPanel from "./features/administration/ProductPanel";
+
+const Home = lazy(() => import("./pages/Home"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Auth = lazy(() => import("./pages/Auth"));
+const Category = lazy(() => import("./pages/Category"));
+const Product = lazy(() => import("./pages/Product"));
+const Search = lazy(() => import("./pages/Search"));
+const WatchList = lazy(() => import("./pages/WatchList"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const Profile = lazy(() => import("./pages/Profile"));
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Spinner = lazy(() => import("./ui/Spinner"));
+const ProtectedRoute = lazy(() => import("./features/authorization/ProtectedRoute"));
+const LoginForm = lazy(() => import("./features/authentication/LoginForm"));
+const RegisterForm = lazy(() => import("./features/authentication/RegisterForm"));
+const CategoryProductList = lazy(() => import("./features/category/CategoryProductList"));
+const UserPanel = lazy(() => import("./features/administration/UserPanel"));
+const CategoryPanel = lazy(() => import("./features/administration/CategoryPanel"));
+const ProductPanel = lazy(() => import("./features/administration/ProductPanel"));
+
+import { updateFullCart } from "./features/cart/cartSlice";
+import { updateFullWatchlist } from "./features/watchlist/watchlistSlice";
 
 
 const queryClient = new QueryClient();
@@ -37,18 +38,7 @@ const queryClient = new QueryClient();
 function App() {
   const user = useSelector(getUser);
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(true);
-  
   useKeepAuthenticated();
-
-  useEffect(() => {
-    const stopLoading = () => {
-      setIsLoading(false);
-    }
-    window.addEventListener("load", stopLoading);
-
-    return () => window.removeEventListener("load", stopLoading);
-  }, []);
 
   useEffect(() => {
     if (!user) {
@@ -63,141 +53,141 @@ function App() {
     }
   }, [user, dispatch]);
 
-  if (isLoading) return <Spinner />
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <GlobalStyles />
         <Router>
-          <Routes>
-            <Route 
-              path="auth"
-              element={
-                <Auth />
-              }
-            >
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route 
+                path="auth"
+                element={
+                  <Auth />
+                }
+              >
+                <Route
+                  index
+                  element={<Navigate to="login" replace />}
+                />
+                <Route 
+                  path="login"
+                  element={<LoginForm />} 
+                />
+                <Route 
+                  path="register"
+                  element={<RegisterForm />}
+                />
+              </Route>
+              <Route 
+                path="/" 
+                element={
+                  <AppLayout>
+                    <Home />
+                  </AppLayout>
+                }
+              />
               <Route
-                index
-                element={<Navigate to="login" replace />}
-              />
-              <Route 
-                path="login"
-                element={<LoginForm />} 
-              />
-              <Route 
-                path="register"
-                element={<RegisterForm />}
-              />
-            </Route>
-            <Route 
-              path="/" 
-              element={
-                <AppLayout>
-                  <Home />
-                </AppLayout>
-              }
-            />
-            <Route
-              path="dashboard"
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["admin"]}>
-                    <Dashboard />
-                  </ProtectedRoute>
-                </AppLayout>
-              } 
-            >
-              <Route
-                index
-                element={<Navigate to="user" replace />}
-              />
-              <Route 
-                index 
-                path="user"
-                element={<UserPanel />}
-              />
+                path="dashboard"
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["admin"]}>
+                      <Dashboard />
+                    </ProtectedRoute>
+                  </AppLayout>
+                } 
+              >
+                <Route
+                  index
+                  element={<Navigate to="user" replace />}
+                />
+                <Route 
+                  index 
+                  path="user"
+                  element={<UserPanel />}
+                />
+                <Route 
+                  path="category"
+                  element={<CategoryPanel />}
+                />
+                <Route 
+                  path="product"
+                  element={<ProductPanel />}
+                />
+              </Route>
               <Route 
                 path="category"
-                element={<CategoryPanel />}
-              />
-              <Route 
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["customer", "admin"]}>
+                      <Category />
+                    </ProtectedRoute>
+                  </AppLayout>
+                }
+              >
+                <Route 
+                  path=":id"
+                  element={<CategoryProductList />}
+                />
+              </Route>
+              <Route
                 path="product"
-                element={<ProductPanel />}
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["customer", "admin"]}>
+                      <Product />
+                    </ProtectedRoute>
+                  </AppLayout>
+                }
               />
-            </Route>
-            <Route 
-              path="category"
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["customer", "admin"]}>
-                    <Category />
-                  </ProtectedRoute>
-                </AppLayout>
-              }
-            >
               <Route 
-                path=":id"
-                element={<CategoryProductList />}
+                path="watchlist" 
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["customer", "admin"]}>
+                      <WatchList />
+                    </ProtectedRoute>
+                  </AppLayout>
+                } 
               />
-            </Route>
-            <Route
-              path="product"
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["customer", "admin"]}>
-                    <Product />
-                  </ProtectedRoute>
-                </AppLayout>
-              }
-            />
-            <Route 
-              path="watchlist" 
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["customer", "admin"]}>
-                    <WatchList />
-                  </ProtectedRoute>
-                </AppLayout>
-              } 
-            />
-            <Route 
-              path="search"
-              element={
-                <AppLayout>
-                  <Search />
-                </AppLayout>
-              }
-            />
-            <Route 
-              path="checkout" 
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["customer", "admin"]}>
-                    <Checkout />
-                  </ProtectedRoute>
-                </AppLayout>
-              } 
-            />
-            <Route 
-              path="profile" 
-              element={
-                <AppLayout>
-                  <ProtectedRoute authorizeRole={["customer", "admin"]}>
-                    <Profile />
-                  </ProtectedRoute>
-                </AppLayout>
-              } 
-            />
-            <Route 
-              path="*" 
-              element={
-                <AppLayout>
-                  <ErrorPage />
-                </AppLayout>
-              }
-            />
-          </Routes>
+              <Route 
+                path="search"
+                element={
+                  <AppLayout>
+                    <Search />
+                  </AppLayout>
+                }
+              />
+              <Route 
+                path="checkout" 
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["customer", "admin"]}>
+                      <Checkout />
+                    </ProtectedRoute>
+                  </AppLayout>
+                } 
+              />
+              <Route 
+                path="profile" 
+                element={
+                  <AppLayout>
+                    <ProtectedRoute authorizeRole={["customer", "admin"]}>
+                      <Profile />
+                    </ProtectedRoute>
+                  </AppLayout>
+                } 
+              />
+              <Route 
+                path="*" 
+                element={
+                  <AppLayout>
+                    <ErrorPage />
+                  </AppLayout>
+                }
+              />
+            </Routes>
+          </Suspense>
         </Router>
         <Toaster 
           position="top-center"
